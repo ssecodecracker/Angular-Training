6dc98b1db378ab1aaa8feaf47966b23749e3f926
Ujjwal <ujjwal.kumar@invesco.com>
2020-04-02 21:14:51 +0530

    Changes for TS

--- 2/TypeScript/src/services/todoService.ts
+++ 2/TypeScript/src/services/todoService.ts
@@ -4,8 +4,31 @@ export class TodoService {
     constructor() {}
 
     getAllTodos(): Promise<TodoType[]> {
-        return new Promise ((resolove, reject) => {
-            setTimeout(resolove, 5000, TODOS);
+        return new Promise ((reslove, reject) => {
+            setTimeout(reslove, 5000, TODOS);
         });
+    }
+
+    getAllCommpletedTodos(): Promise<TodoType[]> {        
+        let completedTodos = [...TODOS];
+        return new Promise ((resolve, reject) => {
+            setTimeout(resolve, 5000, completedTodos = completedTodos.filter(filteredTodo => {
+                return filteredTodo.completed == false;
+            }))
+        })
+    }
+
+    saveTodo(updatedTodos): Promise<String> {
+        TODOS.push(updatedTodos);
+        return new Promise ((resolve, reject) => {
+            setTimeout(resolve, 5000, 'Todo Saved')
+        })
+    }
+
+    updateTodo(indexOfTodo, updatedTodo): Promise<String> {
+        TODOS[indexOfTodo] = {...updatedTodo};
+        return new Promise ((resolve, reject) => {
+            setTimeout(resolve, 5000, 'Todo Updated')
+        })
     }
 }
--- 2/TypeScript/src/todoComponent.ts
+++ 2/TypeScript/src/todoComponent.ts
@@ -3,36 +3,39 @@ import { TodoService } from './services';
 class TodoComponent {
     constructor(private service: TodoService) { }
 
-    private todos = [];
-
     public listAllTodo() {
         this.service.getAllTodos().then(todos => {
-            this.todos = todos;
+            console.log(todos);
         });        
-            
-        console.log(this.todos);
     }
 
-    public saveTodo() {
-
+    public saveTodo(updateTodo = []) {
+        this.service.saveTodo(updateTodo).then(response => {
+            console.log(response);
+        }); 
     }
 
-    public updateTodo() {
-
+    public updateTodo(index, todoToBeUpdated) {
+        this.service.updateTodo(index, todoToBeUpdated).then(response => {
+            console.log(response);
+        }); 
     }
 
     public listAllCompletedTodo() {
-        let completedTodos = [...this.todos];
-        completedTodos = completedTodos.filter(filteredTodo => {
-            return filteredTodo.completed == false;
-        });
-        
-        console.log(completedTodos);
+        this.service.getAllCommpletedTodos().then(completedTodos => {
+            console.log(completedTodos);
+        });  
     }
 }
 
+let todoToBeUpdated =  {
+    "userId": 3,
+    "id": 54,
+    "title": "Updated",
+    "completed": true
+}
 let todos = new TodoComponent(new TodoService());
 todos.listAllTodo();
 todos.saveTodo();
-todos.updateTodo();
+todos.updateTodo(5, todoToBeUpdated);
 todos.listAllCompletedTodo();
